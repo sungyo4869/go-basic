4e66f8af5c6cf9de0825748dce53443b0ae61017
sungyo4869 <hchengjian1@gmail.com>
2024-02-18 00:22:12 +0900

    インデントそろえた

--- handler/middleware/log.go
+++ handler/middleware/log.go
@@ -8,39 +8,39 @@ import (
 )
 
 type LogFormat struct {
-	Timestamp 	time.Time	`json:"timestamp"`
-	Latency		int64		`json:"latency"`
-	Path		string		`json:"Path"`
-	OS			string		`json:"os"`
+	Timestamp time.Time `json:"timestamp"`
+	Latency   int64     `json:"latency"`
+	Path      string    `json:"Path"`
+	OS        string    `json:"os"`
 }
 
 func Log(h http.Handler) http.Handler {
-fn := func(w http.ResponseWriter, r *http.Request) {
-	accessTime := time.Now()
-	
-	h.ServeHTTP(w, r)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		accessTime := time.Now()
 
-	endTime := time.Now()
+		h.ServeHTTP(w, r)
 
-	latency := endTime.Sub(accessTime)
-	path := r.URL.String()
-	os := r.Context().Value(keyOS).(string)
+		endTime := time.Now()
 
-	logData := LogFormat{
-		Timestamp: accessTime,
-		Latency: int64(latency),
-		Path: path,
-		OS: os,
+		latency := endTime.Sub(accessTime)
+		path := r.URL.String()
+		os := r.Context().Value(keyOS).(string)
+
+		logData := LogFormat{
+			Timestamp: accessTime,
+			Latency:   int64(latency),
+			Path:      path,
+			OS:        os,
+		}
+
+		jsonData, err := json.Marshal(logData)
+		if err != nil {
+			fmt.Println("構造体をJSONに変換できませんでした:", err)
+			return
+		}
+		// fmt.Println(logData)
+		fmt.Println(string(jsonData))
+
 	}
-
-	jsonData, err := json.Marshal(logData)
-	if err != nil {
-		fmt.Println("構造体をJSONに変換できませんでした:", err)
-		return
-	}
-	// fmt.Println(logData)
-	fmt.Println(string(jsonData))
-
-}
-return http.HandlerFunc(fn)
+	return http.HandlerFunc(fn)
 }
