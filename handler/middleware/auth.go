da4186425b4c337b4dd6fbc33d05623432a481bb
sungyo4869 <hchengjian1@gmail.com>
2024-02-19 01:27:53 +0900

    basic認証実装できた

--- handler/middleware/auth.go
+++ handler/middleware/auth.go
@@ -2,10 +2,26 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 )
 
 func BasicAuth(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		userID := os.Getenv("BASIC_AUTH_USER_ID")
+		password := os.Getenv("BASIC_AUTH_PASSWORD")
+
+		ui, pass, ok := r.BasicAuth()
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if userID != ui || password != pass {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
 		h.ServeHTTP(w, r)
 	}
 
