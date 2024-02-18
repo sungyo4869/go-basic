72263479c712dfd827d0d1a87a977fbea1884bbf
sungyo4869 <hchengjian1@gmail.com>
2024-02-18 20:06:24 +0900

    必要のない変数を削除した

--- handler/middleware/storeOSName.go
+++ handler/middleware/storeOSName.go
@@ -11,11 +11,9 @@ type ctxKeyOS struct{}
 
 func StoreOSName(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
+		userAgent := useragent.Parse(r.UserAgent())
 
-		userAgent := r.UserAgent()
-		ua := useragent.Parse(userAgent)
-
-		ctx := context.WithValue(r.Context(), ctxKeyOS{}, ua.OS)
+		ctx := context.WithValue(r.Context(), ctxKeyOS{}, userAgent.OS)
 		h.ServeHTTP(w, r.WithContext(ctx))
 	}
 	return http.HandlerFunc(fn)
