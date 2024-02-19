551cacb77a63b4ff56292053f58c82743331894b
sungyo4869 <hchengjian1@gmail.com>
2024-02-19 16:43:43 +0900

    コマンドライン引数で環境変数を受け取るようにした

--- handler/middleware/auth.go
+++ handler/middleware/auth.go
@@ -8,10 +8,11 @@ import (
 func BasicAuth(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
-		userID := os.Getenv("BASIC_AUTH_USER_ID")
-		password := os.Getenv("BASIC_AUTH_PASSWORD")
+		userID := os.Args[1]
+		password := os.Args[2]
 
 		ui, pass, ok := r.BasicAuth()
+ 
 		if !ok {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
