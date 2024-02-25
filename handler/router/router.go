7674da731173ecf4e483d71580fccd9999c19505
sungyo4869 <hchengjian1@gmail.com>
2024-02-25 21:08:58 +0900

    authのミドルウェアを通るように設定した

--- handler/router/router.go
+++ handler/router/router.go
@@ -15,7 +15,7 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 
 	// エンドポイントの定義
 	mux.HandleFunc("/healthz", middleware.Recovery(middleware.StoreOSName(middleware.Log(handler.NewHealthzHandler()))).ServeHTTP)
-	mux.HandleFunc("/todos", middleware.Recovery(middleware.StoreOSName(middleware.Log(handler.NewTODOHandler(service.NewTODOService(todoDB))))).ServeHTTP)
+	mux.HandleFunc("/todos", middleware.Recovery(middleware.StoreOSName(middleware.Log(middleware.BasicAuth(handler.NewTODOHandler(service.NewTODOService(todoDB)))))).ServeHTTP)
 	mux.HandleFunc("/do-panic", middleware.Recovery(middleware.StoreOSName(middleware.Log(handler.NewPanicHandler()))).ServeHTTP)
 
 	return mux
